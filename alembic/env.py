a0a7361ceae007e7525eec90a1e8d0fcfefcd224
Victor Smirnov <csatom@yandex.ru>
2024-02-16 13:23:36 +0300

    changed alembic settings

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from src.model.database import Base
+from src.model.ENV import settings
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,12 +24,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", settings.DATA_BASE_URL)
 
 
 def run_migrations_offline() -> None:
