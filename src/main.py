ae4e9eadb6b3636b57611ef260c238b622923db0
Victor Smirnov <csatom@yandex.ru>
2024-02-16 16:02:00 +0300

    added sceleton of handlers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Form
+
+from typing import Annotated
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def index():
+    return {"message": "главная страница"}
+
+
+@app.get("/currencies")
+async def currencies():
+    return ["list all currencies"]
+
+
+@app.post("/currencies")
+async def currencies(name: Annotated[str, Form()], code: Annotated[str, Form()], sign: Annotated[str, Form()]):
+    return {"name": name, "code": code, "sign": sign}
+
+
+@app.get("/currency/{code}")
+async def currency(code):
+    return {"currency": code}
+
+
+@app.get("/exchangeRates")
+async def exchange_rates():
+    return ["list all exchange_rates"]
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
