d0193c842b919a776ab5cb34aadb77e8a462da29
Victor Smirnov <csatom@yandex.ru>
2024-02-17 19:44:46 +0300

    added config.py

---
+++ src/model/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    DB_HOST: str = getenv('DB_HOST')
+    DB_PORT: int = getenv('DB_PORT')
+    DB_USER: str = getenv('DB_USER')
+    DB_PASS: str = getenv('DB_PASS')
+    DB_NAME: str = getenv('DB_NAME')
+
+    @property
+    def data_base_url(self):
+        # "postgresql+asyncpg://currency_user:0000@localhost:5432/currency_exchange"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+settings = Settings()
