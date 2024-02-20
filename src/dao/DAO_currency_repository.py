9b600d1a7ed5eeeec9c7cce60698080b18e9d874
Victor Smirnov <csatom@yandex.ru>
2024-02-20 18:30:00 +0300

    added find_by_id func currencies

--- src/dao/DAO_currency_repository.py
+++ src/dao/DAO_currency_repository.py
@@ -1,6 +1,8 @@
 from sqlalchemy import select, Result
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from typing import Union
+from src.dto_response import ErrorResponse
 from src.model import Currencies
 
 
@@ -10,13 +12,38 @@ class DaoCurrencyRepository:
     """
 
     @staticmethod
-    async def find_all(session: AsyncSession) -> list[Currencies]:
+    async def find_all(session: AsyncSession) -> Union[list[Currencies], ErrorResponse]:
         """
         Метод возвращает список объектов класса Currencies
         :param session: объект асинхронной сессии AsyncSession
         :return: list[Currencies]
         """
-        stmt = select(Currencies).order_by(Currencies.id)
-        result: Result = await session.execute(stmt)
-        list_all_currencies = result.scalars().all()
-        return list(list_all_currencies)
+        try:
+            stmt = select(Currencies).order_by(Currencies.id)
+            result: Result = await session.execute(stmt)
+            list_all_currencies = result.scalars().all()
+            return list(list_all_currencies)
+        except SQLAlchemyError as e:
+            response = ErrorResponse(code=500, message=f"База данных недоступна: {e}")
+            return response
+
+    @staticmethod
+    async def find_by_id(session: AsyncSession, currency_id: int) -> Union[Currencies, ErrorResponse]:
+        """
+        Метод возвращает найденный объект класса Currencies если он найден в БД, иначе объект ErrorResponse
+        :param session: объект асинхронной сессии AsyncSession
+        :param currency_id: айди валюты
+        :return: объект класса Currencies или ErrorResponse
+        """
+        try:
+            stmt = select(Currencies).where(Currencies.id == currency_id)
+            result: Result = await session.execute(stmt)
+            currency = result.scalars().one()
+            if isinstance(currency, Currencies):
+                return currency
+            else:
+                response = ErrorResponse(code=404, message=f"Валюта с id {currency_id} не найдена")
+                return response
+        except SQLAlchemyError as e:
+            response = ErrorResponse(code=500, message=f"База данных недоступна: {e}")
+            return response
--- src/dto_response/__init__.py
+++ src/dto_response/__init__.py
@@ -3,3 +3,5 @@ __all__ = (
     "ExchangeResponse",
 )
 
+from src.dto_response.error_response import ErrorResponse
+from src.dto_response.exchange_response import ExchangeResponse
