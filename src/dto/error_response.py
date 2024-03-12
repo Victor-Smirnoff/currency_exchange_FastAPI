a9d2cf52523f93e95678725ecd41c9a0cab5ccc7
Victor Smirnov <csatom@yandex.ru>
2024-03-12 19:30:29 +0300

    refactor: ErrorResponse model changed to BaseModel pydantic

--- src/dto/error_response.py
+++ src/dto/error_response.py
@@ -1,14 +1,13 @@
-class ErrorResponse:
+from pydantic import BaseModel
+
+
+class ErrorResponse(BaseModel):
     """
     Класс для хранения данных по ответу с какой-либо ошибкой
     """
-    def __init__(self, code, message):
-        """
-        :param code: код HTTP ответа
-        :param message: сообщение ответа
-        """
-        self.code = code
-        self.message = message
+
+    code: int
+    message: str
 
     def __str__(self):
         return str({"message": self.message, "code": self.code})
