f4f4ef4ee66b3aef3fb6f11692d7747c534d489f
Victor Smirnov <csatom@yandex.ru>
2024-03-12 18:00:47 +0300

    refactor: renamed files view to controller

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -4,6 +4,6 @@ __all__ = (
     "exchange_router",
 )
 
-from src.controller.currencies_views import router as currencies_router
-from src.controller.exchange_rates_views import router as exchange_rates_router
-from src.controller.exchange_view import router as exchange_router
+from src.controller.currencies_controller import router as currencies_router
+from src.controller.exchange_rates_controller import router as exchange_rates_router
+from src.controller.exchange_controller import router as exchange_router
